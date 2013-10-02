6487e1ed6390ad54049a12e643f9116a9ec372ad
Dave Eddy <dave@daveeddy.com>
2013-10-02 14:21:36 -0700

    Update log-timestamp.h

--- log-timestamp.h
+++ log-timestamp.h
@@ -3,6 +3,18 @@
  *
  * usage is the same as printf()
  *
+ * Author: Dave Eddy <dave@daveeddy.com>
+ * License: MIT
+ */
+ 
+ #ifndef _LOG_TIMESTAMP_H
+ #define _LOG_TIMESTAMP_H
+/**
+ * print a timestamped log line
+ *
+ * usage is the same as printf()
+ *
  * returns nothing
  */
 void LOG(const char *fmt, ...);
+#endif
